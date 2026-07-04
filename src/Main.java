3b806a55707f3e72f1e5cc9b92e0a0935fd4df5b
MateusOlm <mateuslimado1234@gmail.com>
2026-07-04 10:27:13 -0300

    (feature): Adicionado as variáveis necessárias para criação do tabuleiro

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
 
 public class Main {
     public static void main(String[] args) {
+        final String horizontal = "\u2500".repeat(3);
+        final String vertical = "\u2502";
+        final String superiorEsquerdo = "\u250C";
+        final String superiorDireito = "\u2510";
+        final String inferiorEsquerdo = "\u2514";
+        final String inferiorDireito = "\u2518";
+        final String juncaoDeLinha = "\u253C";
+        final String juncaoDeLinhaSuperior = "\u252C";
+        final String juncaoDeLinhaInferior = "\u2534";
+        final String juncaoDeLinhaEsquerda = "\u251C";
+        final String juncaoDeLinhaDireita = "\u2524";
+        final String vazio = " ";
 
     }
 }
