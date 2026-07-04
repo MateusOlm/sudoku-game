3733681af68d5d3ddb79d25168f4ef0d1a38a2a0
MateusOlm <mateuslimado1234@gmail.com>
2026-07-04 10:56:47 -0300

    (feature): Adicionado classe para mostrar o tabuleiro

--- src/board/board.java
+++ src/board/board.java
@@ -1,4 +1,69 @@
 package board;
 
 public class board {
+    final String horizontal = "\u2500".repeat(3);
+    final String vertical = "\u2502";
+    final String superiorEsquerdo = "\u250C";
+    final String superiorDireito = "\u2510";
+    final String inferiorEsquerdo = "\u2514";
+    final String inferiorDireito = "\u2518";
+    final String juncaoDeLinha = "\u253C";
+    final String juncaoDeLinhaSuperior = "\u252C";
+    final String juncaoDeLinhaInferior = "\u2534";
+    final String juncaoDeLinhaEsquerda = "\u251C";
+    final String juncaoDeLinhaDireita = "\u2524";
+    final String vazio = " ";
+
+
+    public void tabuleiro(int[][] linhas) {
+        this.superior();
+        this.meio();
+        this.meio();
+        this.inferior();
+    }
+
+    private void linha(int[] linha) {
+        for(int numero: linha) {
+            System.out.print(vertical);
+            this.mostrarNumero(numero);
+            System.out.print(vertical);
+        }
+        System.out.println();
+    }
+
+    private void mostrarNumero(int numero) {
+        System.out.print(vazio);
+        System.out.print(numero);
+        System.out.print(vazio);
+    }
+
+    private void superior() {
+        System.out.print(superiorEsquerdo);
+        System.out.print(horizontal);
+        System.out.print(juncaoDeLinhaSuperior);
+        System.out.print(horizontal);
+        System.out.print(juncaoDeLinhaSuperior);
+        System.out.print(horizontal);
+        System.out.println(superiorDireito);
+    }
+
+    private void meio() {
+        System.out.print(juncaoDeLinhaEsquerda);
+        System.out.print(horizontal);
+        System.out.print(juncaoDeLinha);
+        System.out.print(horizontal);
+        System.out.print(juncaoDeLinha);
+        System.out.print(horizontal);
+        System.out.println(juncaoDeLinhaDireita);
+    }
+
+    private void inferior() {
+        System.out.print(inferiorEsquerdo);
+        System.out.print(horizontal);
+        System.out.print(juncaoDeLinhaInferior);
+        System.out.print(horizontal);
+        System.out.print(juncaoDeLinhaInferior);
+        System.out.print(horizontal);
+        System.out.println(inferiorDireito);
+    }
 }
