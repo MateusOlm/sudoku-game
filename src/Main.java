323aa3f837ab4cace8afffc6212599f555ac6bdb
MateusOlm <mateuslimado1234@gmail.com>
2026-07-06 18:11:23 -0300

    (test): Testado a funcionalidade de adicionar número no tabuleiro

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,41 @@
 import board.Board;
+import game.GameRules;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-    int[][] linhas = {
-            {3, 8, 4, 3, 8, 4, 3, 8, 4},
-            {2, 7, 5, 2, 7, 5, 2, 7, 5},
-            {9, 6, 1, 9, 6, 1, 9, 6, 1}
-    };
+        Scanner sc = new Scanner(System.in);
+        Board sudoku = Board.create();
+        GameRules regras = GameRules.create();
+        int numeroEscolhido;
+        String escolha;
 
-    Board sudoku = Board.create();
+        int[][] linhas = {
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0}
+        };
 
-    sudoku.tabuleiro(linhas);
+        while (true) {
+            sudoku.tabuleiro(regras.getLinhas());
+            System.out.print("Digite o número que quer adicionar (entre 1 e 9): ");
+            numeroEscolhido = sc.nextInt();
+
+            System.out.print("Digite a linha e a coluna onde quer adiconar (Ex.: 7B): ");
+            escolha = sc.next();
+
+            regras.alterarTabuleiro(escolha, numeroEscolhido);
+
+            sudoku.clear();
+        }
+
     }
 }
