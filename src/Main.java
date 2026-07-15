b9ad34c8c3ea75de1fdeb98c4b9554f462582d90
MateusOlm <mateuslimado1234@gmail.com>
2026-07-14 23:09:57 -0300

    (test): Testado as alterações do lanterna com possibilidade mover pela tabuleiro e alterar um número no tabuleiro

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,72 @@
 import board.Board;
 import game.GameRules;
 
-import java.util.Scanner;
+import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
 
 public class Main {
+
     public static void main(String[] args) {
+        try {
+            Terminal terminal = new DefaultTerminalFactory().createTerminal();
+            Screen screen = new TerminalScreen(terminal);
+            screen.startScreen();
 
-        Scanner sc = new Scanner(System.in);
-        Board sudoku = Board.create();
-        GameRules regras = GameRules.create();
-        int numeroEscolhido;
-        String escolha;
+            terminal.setCursorVisible(false);
 
-        while (true) {
-            sudoku.tabuleiro(regras.getLinhas());
+            TextGraphics tg = screen.newTextGraphics();
+            Board sudoku = Board.create();
+            GameRules regras = GameRules.create();
+            boolean rodando = true;
 
-            System.out.print("Digite o número que quer adicionar (entre 1 e 9): ");
-            numeroEscolhido = sc.nextInt();
+            while (rodando) {
+                screen.clear();
 
-            System.out.print("Digite a linha e a coluna onde quer adiconar (Ex.: 7B): ");
-            escolha = sc.next();
+                int[] posicaoAtual = regras.getPosicaoAtual();
 
-            regras.alterarTabuleiro(escolha, numeroEscolhido);
+                sudoku.tabuleiro(tg, regras.getLinhas(), posicaoAtual);
 
-            sudoku.clear();
+                screen.refresh();
+
+                KeyStroke tecla = screen.readInput();
+
+                if (tecla.getKeyType() == KeyType.Escape) {
+                    rodando = false;
+                } else if (tecla.getKeyType() == KeyType.ArrowUp) {
+                    regras.alterarPosicaoAtual(posicaoAtual[0] - 1, posicaoAtual[1]);
+                } else if (tecla.getKeyType() == KeyType.ArrowDown) {
+                    regras.alterarPosicaoAtual(posicaoAtual[0] + 1, posicaoAtual[1]);
+                } else if (tecla.getKeyType() == KeyType.ArrowLeft) {
+                    regras.alterarPosicaoAtual(posicaoAtual[0], posicaoAtual[1] - 1);
+                } else if (tecla.getKeyType() == KeyType.ArrowRight) {
+                    regras.alterarPosicaoAtual(posicaoAtual[0], posicaoAtual[1] + 1);
+                }
+
+                else if (tecla.getKeyType() == KeyType.Character) {
+                    char caractere = tecla.getCharacter();
+
+                    if (caractere >= '1' && caractere <= '9') {
+                        int numeroDigitado = Character.getNumericValue(caractere);
+
+                        regras.alterarTabuleiro(numeroDigitado);
+                    }
+                    else if (caractere == ' ' || tecla.getKeyType() == KeyType.Backspace) {
+                        // Lógica para limpar a célula atual do Sudoku (colocar 0)
+                    }
+                }
+            }
+            
+            screen.stopScreen();
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
