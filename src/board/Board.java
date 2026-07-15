2e7dc18d8bfbe39db1cb245d6f79900056b006e3
MateusOlm <mateuslimado1234@gmail.com>
2026-07-14 23:06:46 -0300

    (feature): Alterado toda geração do tabuleiro para o lanterna

--- src/board/Board.java
+++ src/board/Board.java
@@ -1,148 +1,220 @@
 package board;
 
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.TerminalSize;
+
 public class Board {
 
     private static final int TAMANHO_BLOCO = 3;
-    private static final String ALFABETO = "  A   B   C   D   E   F   G   H   I";
-    private int numero = 1;
 
-    private Board() {
-    }
+    private static final int LARGURA_TABULEIRO = 37;
+    private static final int ALTURA_TABULEIRO = 19;
+
+    private int cursorY = 0;
+    private int offsetX = 0;
+
+    private Board() {}
 
     public static Board create() {
         return new Board();
     }
 
-    private void linha(int[] linha) {
-        System.out.print(this.numero);
-        System.out.print(BoxDrawing.ESPACO);
-        System.out.print(BoxDrawing.VERTICAL_PESADO);
-        for(int i = 0; i < linha.length; i++) {
-            this.mostrarNumero(linha[i]);
-            if ((i + 1) % TAMANHO_BLOCO == 0) {
-                System.out.print(BoxDrawing.VERTICAL_PESADO);
+    private void resetCursor(TextGraphics tg) {
+        TerminalSize tamanhoTerminal = tg.getSize();
+        int larguraTerminal = tamanhoTerminal.getColumns();
+        int alturaTerminal = tamanhoTerminal.getRows();
+
+        this.offsetX = Math.max(0, (larguraTerminal - LARGURA_TABULEIRO) / 2);
+        this.cursorY = Math.max(0, (alturaTerminal - ALTURA_TABULEIRO) / 2);
+    }
+
+    private void linha(TextGraphics tg, int[] linha, int[] posicaoAtual) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(BoxDrawing.VERTICAL_PESADO);
+
+        // Aplica o deslocamento horizontal (offsetX) no início da linha
+        tg.putString(offsetX, cursorY, sb.toString());
+        int cursorX = offsetX + sb.length();
+
+        for (int i = 0; i < linha.length; i++) {
+            if (i == posicaoAtual[1]) {
+                mostrarNumeroComFundo(tg, cursorX, linha[i]);
             } else {
-                System.out.print(BoxDrawing.VERTICAL_LEVE);
+                mostrarNumero(tg, cursorX, linha[i]);
             }
+            cursorX += 3;
+
+            String div = ((i + 1) % TAMANHO_BLOCO == 0) ? BoxDrawing.VERTICAL_PESADO : BoxDrawing.VERTICAL_LEVE;
+            tg.putString(cursorX, cursorY, div);
+            cursorX += div.length();
         }
-        this.incrementoNumero();
-        System.out.println();
+
+        cursorY++;
     }
 
-    private void incrementoNumero() {
-        if (numero == 9) {
-            this.numero = 1;
-        }else {
-            this.numero ++;
+    private void linha(TextGraphics tg, int[] linha) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(BoxDrawing.VERTICAL_PESADO);
+
+        // Aplica o deslocamento horizontal (offsetX) no início da linha
+        tg.putString(offsetX, cursorY, sb.toString());
+        int cursorX = offsetX + sb.length();
+
+        for (int i = 0; i < linha.length; i++) {
+            mostrarNumeroComFundo(tg, cursorX, linha[i]);
+            cursorX += 3;
+
+            String div = ((i + 1) % TAMANHO_BLOCO == 0) ? BoxDrawing.VERTICAL_PESADO : BoxDrawing.VERTICAL_LEVE;
+            tg.putString(cursorX, cursorY, div);
+            cursorX += div.length();
         }
+
+        cursorY++;
     }
 
-    private void mostrarNumero(int numero) {
-        System.out.print(BoxDrawing.ESPACO);
-        System.out.print(numero == 0 ? BoxDrawing.ESPACO : numero);
-        System.out.print(BoxDrawing.ESPACO);
+    private void mostrarNumeroComFundo(TextGraphics tg, int x, int numero) {
+        tg.setBackgroundColor(TextColor.Indexed.fromRGB(128, 128, 128)); // Equivalente ao cinza 244
+        tg.setForegroundColor(TextColor.ANSI.WHITE);
+
+        String nStr = (numero == 0) ? BoxDrawing.ESPACO : String.valueOf(numero);
+        tg.putString(x, cursorY, BoxDrawing.ESPACO + nStr + BoxDrawing.ESPACO);
+
+        tg.setBackgroundColor(TextColor.ANSI.DEFAULT);
+        tg.setForegroundColor(TextColor.ANSI.DEFAULT);
     }
 
-    public void clear() {
-        System.out.print(BoxDrawing.CLEAR);
-        System.out.flush();
+    private void mostrarNumero(TextGraphics tg, int x, int numero) {
+        String nStr = (numero == 0) ? BoxDrawing.ESPACO : String.valueOf(numero);
+        tg.putString(x, cursorY, BoxDrawing.ESPACO + nStr + BoxDrawing.ESPACO);
     }
 
-    public void tabuleiro(int[][] linhas) {
-        this.superior();
+    public void tabuleiro(TextGraphics tg, int[][] linhas, int[] posicaoAtual) {
+        this.resetCursor(tg); // Passamos o TextGraphics para ler o tamanho atual do terminal
+
+        this.superior(tg);
         for (int i = 0; i < linhas.length - 1; i++) {
             if ((i + 1) % TAMANHO_BLOCO == 0) {
-                this.linha(linhas[i]);
-                this.meioExterno();
+                if (i == posicaoAtual[0]) {
+                    this.linha(tg, linhas[i]);
+                } else {
+                    this.linha(tg, linhas[i], posicaoAtual);
+                }
+                this.meioExterno(tg);
             } else {
-                tabuleiroRepete(linhas[i]);
+                if (i == posicaoAtual[0]) {
+                    tabuleiroRepete(tg, linhas[i]);
+                } else {
+                    tabuleiroRepeteSemFundo(tg, linhas[i], posicaoAtual);
+                }
             }
         }
-        this.linha(linhas[linhas.length - 1]);
-        this.inferior();
+        if (posicaoAtual[0] == 8) {
+            this.linha(tg, linhas[linhas.length - 1]);
+        } else {
+            this.linha(tg, linhas[linhas.length - 1], posicaoAtual);
+        }
+        this.inferior(tg);
     }
 
-    private void tabuleiroRepete(int[] linhas) {
-        this.linha(linhas);
-        this.meioInterno();
+    private void tabuleiroRepeteSemFundo(TextGraphics tg, int[] linhas, int[] posicaoAtual) {
+        this.linha(tg, linhas, posicaoAtual);
+        this.meioInterno(tg);
     }
 
-    private void superior() {
-        System.out.print(BoxDrawing.ESPACO_DOUBLE);
-        System.out.print(BoxDrawing.CANTO_BAIXO_DIREITA_PESADO);
-        this.superiorRepete();
-        System.out.print(BoxDrawing.JUNCAO_BAIXO_ESQUERDA_DIREITA_PESADO);
-        this.superiorRepete();
-        System.out.print(BoxDrawing.JUNCAO_BAIXO_ESQUERDA_DIREITA_PESADO);
-        this.superiorRepete();
-        System.out.println(BoxDrawing.CANTO_BAIXO_ESQUERDA_PESADO);
+    private void tabuleiroRepete(TextGraphics tg, int[] linhas) {
+        this.linha(tg, linhas);
+        this.meioInterno(tg);
     }
 
-    private void superiorRepete() {
-        System.out.print(BoxDrawing.HORIZONTAL_PESADO);
-        System.out.print(BoxDrawing.BAIXO_PESADO_ESQUERDA_DIREITA);
-        System.out.print(BoxDrawing.HORIZONTAL_PESADO);
-        System.out.print(BoxDrawing.BAIXO_PESADO_ESQUERDA_DIREITA);
-        System.out.print(BoxDrawing.HORIZONTAL_PESADO);
+    private void superior(TextGraphics tg) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(BoxDrawing.CANTO_BAIXO_DIREITA_PESADO);
+        sb.append(superiorRepeteString());
+        sb.append(BoxDrawing.JUNCAO_BAIXO_ESQUERDA_DIREITA_PESADO);
+        sb.append(superiorRepeteString());
+        sb.append(BoxDrawing.JUNCAO_BAIXO_ESQUERDA_DIREITA_PESADO);
+        sb.append(superiorRepeteString());
+        sb.append(BoxDrawing.CANTO_BAIXO_ESQUERDA_PESADO);
+
+        tg.putString(offsetX, cursorY, sb.toString());
+        cursorY++;
     }
 
-    private void inferior() {
-        System.out.print(BoxDrawing.ESPACO_DOUBLE);
-        System.out.print(BoxDrawing.CANTO_CIMA_DIREITA_PESADO);
-        this.inferiorRepete();
-        System.out.print(BoxDrawing.JUNCAO_CIMA_ESQUERDA_DIREITA_PESADO);
-        this.inferiorRepete();
-        System.out.print(BoxDrawing.JUNCAO_CIMA_ESQUERDA_DIREITA_PESADO);
-        this.inferiorRepete();
-        System.out.println(BoxDrawing.CANTO_CIMA_ESQUERDA_PESADO);
-        System.out.print(BoxDrawing.ESPACO_DOUBLE);
-        System.out.println(ALFABETO);
+    private String superiorRepeteString() {
+        return BoxDrawing.HORIZONTAL_PESADO +
+                BoxDrawing.BAIXO_PESADO_ESQUERDA_DIREITA +
+                BoxDrawing.HORIZONTAL_PESADO +
+                BoxDrawing.BAIXO_PESADO_ESQUERDA_DIREITA +
+                BoxDrawing.HORIZONTAL_PESADO;
     }
 
-    private void inferiorRepete() {
-        System.out.print(BoxDrawing.HORIZONTAL_PESADO);
-        System.out.print(BoxDrawing.CIMA_PESADO_ESQUERDA_DIREITA);
-        System.out.print(BoxDrawing.HORIZONTAL_PESADO);
-        System.out.print(BoxDrawing.CIMA_PESADO_ESQUERDA_DIREITA);
-        System.out.print(BoxDrawing.HORIZONTAL_PESADO);
+    private void inferior(TextGraphics tg) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(BoxDrawing.CANTO_CIMA_DIREITA_PESADO);
+        sb.append(inferiorRepeteString());
+        sb.append(BoxDrawing.JUNCAO_CIMA_ESQUERDA_DIREITA_PESADO);
+        sb.append(inferiorRepeteString());
+        sb.append(BoxDrawing.JUNCAO_CIMA_ESQUERDA_DIREITA_PESADO);
+        sb.append(inferiorRepeteString());
+        sb.append(BoxDrawing.CANTO_CIMA_ESQUERDA_PESADO);
+
+        tg.putString(offsetX, cursorY, sb.toString());
+        cursorY++;
+
+        // Caso ainda queira deixar o espaçamento duplo abaixo do tabuleiro de forma centralizada
+        tg.putString(offsetX, cursorY, BoxDrawing.ESPACO_DOUBLE);
     }
 
-    private void meioInterno() {
-        System.out.print(BoxDrawing.ESPACO_DOUBLE);
-        System.out.print(BoxDrawing.VERTICAL_PESADA_DIREITA);
-        this.meioInternoRepete();
-        System.out.print(BoxDrawing.QUATRO_DIRECOES_VERTICAL_LEVE);
-        this.meioInternoRepete();
-        System.out.print(BoxDrawing.QUATRO_DIRECOES_VERTICAL_LEVE);
-        this.meioInternoRepete();
-        System.out.println(BoxDrawing.VERTICAL_PESADA_ESQUERDA);
+    private String inferiorRepeteString() {
+        return BoxDrawing.HORIZONTAL_PESADO +
+                BoxDrawing.CIMA_PESADO_ESQUERDA_DIREITA +
+                BoxDrawing.HORIZONTAL_PESADO +
+                BoxDrawing.CIMA_PESADO_ESQUERDA_DIREITA +
+                BoxDrawing.HORIZONTAL_PESADO;
     }
 
-    private void meioInternoRepete() {
-        System.out.print(BoxDrawing.HORIZONTAL_LEVE);
-        System.out.print(BoxDrawing.JUNCAO_QUATRO_DIRECOES_LEVE);
-        System.out.print(BoxDrawing.HORIZONTAL_LEVE);
-        System.out.print(BoxDrawing.JUNCAO_QUATRO_DIRECOES_LEVE);
-        System.out.print(BoxDrawing.HORIZONTAL_LEVE);
+    private void meioInterno(TextGraphics tg) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(BoxDrawing.VERTICAL_PESADA_DIREITA);
+        sb.append(meioInternoRepeteString());
+        sb.append(BoxDrawing.QUATRO_DIRECOES_VERTICAL_LEVE);
+        sb.append(meioInternoRepeteString());
+        sb.append(BoxDrawing.QUATRO_DIRECOES_VERTICAL_LEVE);
+        sb.append(meioInternoRepeteString());
+        sb.append(BoxDrawing.VERTICAL_PESADA_ESQUERDA);
+
+        tg.putString(offsetX, cursorY, sb.toString());
+        cursorY++;
     }
 
-    private void meioExterno() {
-        System.out.print(BoxDrawing.ESPACO_DOUBLE);
-        System.out.print(BoxDrawing.JUNCAO_CIMA_BAIXO_DIREITA_PESADO);
-        this.meioExternoRepete();
-        System.out.print(BoxDrawing.JUNCAO_QUATRO_DIRECOES_PESADO);
-        this.meioExternoRepete();
-        System.out.print(BoxDrawing.JUNCAO_QUATRO_DIRECOES_PESADO);
-        this.meioExternoRepete();
-        System.out.println(BoxDrawing.JUNCAO_CIMA_BAIXO_ESQUERDA_PESADO);
-
+    private String meioInternoRepeteString() {
+        return BoxDrawing.HORIZONTAL_LEVE +
+                BoxDrawing.JUNCAO_QUATRO_DIRECOES_LEVE +
+                BoxDrawing.HORIZONTAL_LEVE +
+                BoxDrawing.JUNCAO_QUATRO_DIRECOES_LEVE +
+                BoxDrawing.HORIZONTAL_LEVE;
     }
 
-    private void meioExternoRepete() {
-        System.out.print(BoxDrawing.HORIZONTAL_PESADO);
-        System.out.print(BoxDrawing.QUATRO_DIRECOES_PESADAS);
-        System.out.print(BoxDrawing.HORIZONTAL_PESADO);
-        System.out.print(BoxDrawing.QUATRO_DIRECOES_PESADAS);
-        System.out.print(BoxDrawing.HORIZONTAL_PESADO);
+    private void meioExterno(TextGraphics tg) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(BoxDrawing.JUNCAO_CIMA_BAIXO_DIREITA_PESADO);
+        sb.append(meioExternoRepeteString());
+        sb.append(BoxDrawing.JUNCAO_QUATRO_DIRECOES_PESADO);
+        sb.append(meioExternoRepeteString());
+        sb.append(BoxDrawing.JUNCAO_QUATRO_DIRECOES_PESADO);
+        sb.append(meioExternoRepeteString());
+        sb.append(BoxDrawing.JUNCAO_CIMA_BAIXO_ESQUERDA_PESADO);
+
+        tg.putString(offsetX, cursorY, sb.toString());
+        cursorY++;
+    }
+
+    private String meioExternoRepeteString() {
+        return BoxDrawing.HORIZONTAL_PESADO +
+                BoxDrawing.QUATRO_DIRECOES_PESADAS +
+                BoxDrawing.HORIZONTAL_PESADO +
+                BoxDrawing.QUATRO_DIRECOES_PESADAS +
+                BoxDrawing.HORIZONTAL_PESADO;
     }
 }
--- src/board/BoxDrawing.java
+++ src/board/BoxDrawing.java
@@ -5,6 +5,9 @@ public class BoxDrawing {
     private BoxDrawing() {}
 
     public static final String ESPACO = " ";
+    public static final String VERMELHO = "\033[48;5;196m";
+    public static final String CINZA = "\033[48;5;244m";
+    public static final String RESET = "\033[0m";
     public static final String ESPACO_DOUBLE = " ".repeat(2);
     public static final String CLEAR =
             "\033[3J" + // limpa o histórico
@@ -12,35 +15,34 @@ public class BoxDrawing {
                     "\033[H";   // cursor no canto superior esquerdo
 
     // Horizontal e vertical leve
-    public static final String HORIZONTAL_LEVE = "\u2500".repeat(3);
-    public static final String VERTICAL_LEVE = "\u2502";
+    public static final String HORIZONTAL_LEVE = "─".repeat(3);
+    public static final String VERTICAL_LEVE = "│";
 
     // Horizontal e vertical pesado
-    public static final String HORIZONTAL_PESADO = "\u2501".repeat(3);
-    public static final String VERTICAL_PESADO = "\u2503";
+    public static final String HORIZONTAL_PESADO = "━".repeat(3);
+    public static final String VERTICAL_PESADO = "┃";
 
     // Junções Leves
-    public static final String JUNCAO_QUATRO_DIRECOES_LEVE = "\u253C";          // ┼// ┤
+    public static final String JUNCAO_QUATRO_DIRECOES_LEVE = "┼";
 
     // Cantos Pesados
-    public static final String CANTO_BAIXO_DIREITA_PESADO = "\u250F";   // ┏
-    public static final String CANTO_BAIXO_ESQUERDA_PESADO = "\u2513";  // ┓
-    public static final String CANTO_CIMA_DIREITA_PESADO = "\u2517";    // ┗
-    public static final String CANTO_CIMA_ESQUERDA_PESADO = "\u251B";   // ┛
+    public static final String CANTO_BAIXO_DIREITA_PESADO = "┏";   // ┏
+    public static final String CANTO_BAIXO_ESQUERDA_PESADO = "┓";  // ┓
+    public static final String CANTO_CIMA_DIREITA_PESADO = "┗";    // ┗
+    public static final String CANTO_CIMA_ESQUERDA_PESADO = "┛";   // ┛
 
     // Junções Pesados
-    public static final String JUNCAO_QUATRO_DIRECOES_PESADO = "\u254B";          // ╋
-    public static final String JUNCAO_BAIXO_ESQUERDA_DIREITA_PESADO = "\u2533";   // ┳
-    public static final String JUNCAO_CIMA_ESQUERDA_DIREITA_PESADO = "\u253B";    // ┻
-    public static final String JUNCAO_CIMA_BAIXO_DIREITA_PESADO = "\u2523";       // ┣
-    public static final String JUNCAO_CIMA_BAIXO_ESQUERDA_PESADO = "\u252B";      // ┫
+    public static final String JUNCAO_QUATRO_DIRECOES_PESADO = "╋";          // ╋
+    public static final String JUNCAO_BAIXO_ESQUERDA_DIREITA_PESADO = "┳";   // ┳
+    public static final String JUNCAO_CIMA_ESQUERDA_DIREITA_PESADO = "┻";    // ┻
+    public static final String JUNCAO_CIMA_BAIXO_DIREITA_PESADO = "┣";       // ┣
+    public static final String JUNCAO_CIMA_BAIXO_ESQUERDA_PESADO = "┫";      // ┫
 
     //Junções pesadas e leves
-    public static final String VERTICAL_PESADA_DIREITA = "\u2520"; // ┠
-    public static final String VERTICAL_PESADA_ESQUERDA = "\u2528"; // ┨
-    public static final String BAIXO_PESADO_ESQUERDA_DIREITA = "\u252F"; // ┯
-    public static final String CIMA_PESADO_ESQUERDA_DIREITA = "\u2537"; // ┷
-    public static final String QUATRO_DIRECOES_PESADAS = "\u253F"; // ┿// ┸
-    public static final String QUATRO_DIRECOES_VERTICAL_LEVE = "\u2542"; // ╂
-
+    public static final String VERTICAL_PESADA_DIREITA = "┠"; // ┠
+    public static final String VERTICAL_PESADA_ESQUERDA = "┨"; // ┨
+    public static final String BAIXO_PESADO_ESQUERDA_DIREITA = "┯"; // ┯
+    public static final String CIMA_PESADO_ESQUERDA_DIREITA = "┷"; // ┷
+    public static final String QUATRO_DIRECOES_PESADAS = "┿"; // ┿// ┸
+    public static final String QUATRO_DIRECOES_VERTICAL_LEVE = "╂"; // ╂
 }
